4dd76321f4fdd7877364efe766b6487cce320f13
Bishaka Samuel <sbishaka@gmail.com>
2015-10-04 14:34:48 +0300

    Add crappy more card

--- preview/app.js
+++ preview/app.js
@@ -11,7 +11,16 @@ var moviestoday = angular.module('moviestoday',[])
     delete $httpProvider.defaults.headers.common["X-Requested-With"];
 }])
 
-.controller('appCtrl',['$scope','$http',function($scope,$http){
+.controller('appCtrl',['$scope','$http','urlSvs','$sce',function($scope,$http, urlSvs,$sce){
+
+        var host = urlSvs.getApiHost();
+
+        $scope.getYouTubeEmbed = function(url){
+            console.log("Youtube url found : " + url);
+            var embed_url = url.replace("watch?v=","embed/");
+            console.log("Youtube embed url returned : " + embed_url);
+            return $sce.trustAsResourceUrl(embed_url);
+        };
 
         $scope.cinemax = {};
         $scope.cinemax.csoon = [
@@ -21,11 +30,23 @@ var moviestoday = angular.module('moviestoday',[])
             {"title":"Loading","poster":"http://ia.media-imdb.com/images/M/MV5BMTUyNjE5NjI5OF5BMl5BanBnXkFtZTgwNzYzMzU3NjE@._V1_SX640_SY720_.jpg"},
         ];
 
-        $http.get('http://bt-njsmoviestoday.rhcloud.com/wscinemax/comingsoon',{withCredentials : true}).success(function(data){
+        $http.get(host+'/wscinemax/comingsoon',{withCredentials : true}).success(function(data){
             $scope.cinemax.csoon = data;
         });
 
-        $http.get('http://bt-njsmoviestoday.rhcloud.com/wscinemax/nowshowing',{withCredentials : true}).success(function(data){
+        $http.get(host+'/wscinemax/nowshowing',{withCredentials : true}).success(function(data){
             $scope.cinemax.nshow = data;
         });
+}])
+
+
+.service('urlSvs',[function(){
+      var m2de_api_host = "http://bt-njsmoviestoday.rhcloud.com";
+      var _gut = {};
+
+      _gut.getApiHost = function(){
+          return m2de_api_host;
+      };
+
+      return _gut;
 }]);
